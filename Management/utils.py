0772383f9784da0f5fc492bb7d0d4b54de3ae14f
Jordan Rose <JordanRose@dal.ca>
2019-01-31 15:03:13 -0400

    Running test on bigdata2

--- Management/utils.py
+++ Management/utils.py
@@ -1,6 +1,6 @@
 from Management.models import Trajectory, TrajectoryFeature,Database, POI_ROI
 import pandas as pd
-from shapely.geometry import Point, shape, Polygon,MultiPolygon,MultiPoint,LineString,MultiLineString
+from shapely.geometry import Point, shape, Polygon,MultiPolygon,MultiPoint,LineString,MultiLineString,mapping
 from .models import TrajectoryFeature, Trajectory
 from trajectory_library import Trajectory as tr
 from trajectory_library.TrajectoryDescriptorFeature import TrajectoryDescriptorFeature
@@ -9,7 +9,10 @@ import json,geojson,fiona
 from math import radians, cos, sin, asin, sqrt
 from geopandas import GeoDataFrame,overlay,GeoSeries
 import numpy as np
+import copy
 from datetime import datetime
+from sys import getsizeof
+
 
 def save_trajectory(file, tid, lat, lon, time, delimiter,db, pois_rois):
 
@@ -48,8 +51,9 @@ def save_trajectory(file, tid, lat, lon, time, delimiter,db, pois_rois):
                 name += "_intersects"
                 new_col = find_intersects(points_geoseries, layer)
             else:
+                #points_geoseries = simplify_linestring(points_geoseries)
                 name += "_shortest_distance"
-                new_col = find_shortest_distance(points_geoseries, layer)
+                new_col = find_shortest_distance(points_geoseries, layer,0.001)
             new_col = list(new_col)
             df[name] = new_col
         df = df.drop(['lon','lat'],axis=1)
@@ -64,7 +68,8 @@ def save_poi_roi(file, name,db):
         layer.type = POI_ROI.ROI
     else:
         layer.type = POI_ROI.POI
-    data = json.loads(file_contents)
+    #data = json.loads(file_contents)
+    data = simplify_linestring(file_contents)
     layer.geojson = data
     layer.db = db
     layer.name = name
@@ -84,18 +89,16 @@ def find_intersects(points_geoseries, roi):
             elif type(s) == MultiPolygon:
                 polygons += list(s)
             else:
-
-
                 raise Exception("Layer contains more than polygons and MultiPolygons")
-    mpolygon = MultiPolygon(polygons)
-    multipolygons = [mpolygon]*len(points_geoseries)
-    polygons_geoseries = GeoSeries(multipolygons)
+    #mpolygon = MultiPolygon(polygons)
+    #multipolygons = [mpolygon]*len(points_geoseries)
+    polygons_geoseries = GeoSeries(polygons)
     polygons_geoseries.crs = fiona.crs.from_epsg(4326)
-    intersection = points_geoseries.within(polygons_geoseries)*1
+    intersection = points_geoseries.within(polygons_geoseries.unary_union)*1
     return intersection
 
 
-def find_shortest_distance(points, poi):
+def find_shortest_distance(points, poi,starting_radius):
     #raise Exception("Not implemented yet")
     min_dist_list = []
     geo = geojson.loads(json.dumps(poi.geojson))
@@ -114,29 +117,49 @@ def find_shortest_distance(points, poi):
         points_interest_geoseries = GeoSeries(points_interest)
         points_interest_geoseries.crs = fiona.crs.from_epsg(4326)
 
-    return [min_dist(p,points_interest_geoseries) for p in points]
-    """
-    for point in points:
-        min = 1000000000
-        for p in poi:
-            hav_dist = haversine_distance(point.y,point.x,p.y,p.x) # ???
-            if hav_dist < min:
-                min = hav_dist
-        min_dist_list.append(min)
-    return min_dist_list
-    """
-def min_dist(point_to_check,points):
+    min_dists = []
+    radius = starting_radius
+    #new_calc,old_calc = [], []
+    for p in points:
+        #d = datetime.now()
+        dist,radius = min_dist(p,points_interest_geoseries,radius)
+        #new_calc.append(datetime.now()-d)
+        #d=datetime.now()
+        #min_dist2(p,points_interest_geoseries)
+        #old_calc.append(datetime.now()-d)
+        min_dists.append(dist)
+    #print(np.mean(new_calc))
+    #print(np.mean(old_calc))
+    return min_dists
+
+
+def min_dist(point_to_check,points,radius):
     #print(point_to_check)
+    within_radius, radius = get_points_by_radius(point_to_check,points,radius)
+    return within_radius.distance(point_to_check).min(),radius
+
+
+def min_dist2(point_to_check,points):
     return points.distance(point_to_check).min()
 
 
-#https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-def haversine_distance(lat1,lon1,lat2,lon2):
-    lat1,lon1,lat2,lon2 = map(radians,[lat1,lon1,lat2,lon2])
-    a = sin(lat2-lat1/2)**2 + cos(lat2) * sin(lon2-lon1/2)**2
-    r = 6371
-    return (2 * asin(sqrt(a))) * r
+def min_dist3(point_to_check,points):
+    return
 
+
+def get_points_by_radius(point_to_check,points,n):
+    intersection_points = GeoSeries()
+    #print("Radius: %.3f" % n)
+    while len(intersection_points)==0:
+        circle = point_to_check.buffer(n)
+        intersection_points = points.where(points.within(circle))
+        intersection_points = intersection_points[intersection_points.notnull()]
+        #print("%.3f %d" % (n,len(intersection_points)))
+        if len(intersection_points)==0:
+            n*=1.25
+        elif len(intersection_points)>50:
+            n/=1.1
+    return intersection_points,n
 
 def save_point_features(df, traj):
     feats = []
@@ -165,3 +188,33 @@ def save_point_features(df, traj):
             #print(e)
             #print("Column %s was not added" % col)
     TrajectoryFeature.objects.bulk_create(feats)
+
+
+def simplify_linestring(gson):
+    loaded_geo = geojson.loads(gson)
+    new_feature_collection = copy.deepcopy(loaded_geo)
+    new_feature_collection.features = []
+    pointsa = 0
+    pointsb = 0
+    #print(getsizeof(loaded_geo))
+    for geometry in loaded_geo.features:
+        if type(geometry.geometry) is geojson.LineString or type(geometry.geometry) is geojson.MultiLineString:
+            line = shape(geometry.geometry)
+            feature = copy.deepcopy(geometry)
+            pointsa+=len(line.coords)
+            line = line.simplify(0.05,preserve_topology=False)
+            pointsb+=len(line.coords)
+
+            feature.geometry = mapping(line)
+            new_feature_collection.features.append(feature)
+    print(pointsa)
+    print(pointsb)
+    return new_feature_collection
+"""
+#https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+def haversine_distance(lat1,lon1,lat2,lon2):
+    lat1,lon1,lat2,lon2 = map(radians,[lat1,lon1,lat2,lon2])
+    a = sin(lat2-lat1/2)**2 + cos(lat2) * sin(lon2-lon1/2)**2
+    r = 6371
+    return (2 * asin(sqrt(a))) * r
+"""
